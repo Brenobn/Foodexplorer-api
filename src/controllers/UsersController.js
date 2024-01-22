4e60614d9e6f859d54826871a9bd9c163e5045aa
Brenobn <brenobragabn@gmail.com>
2024-01-22 20:16:05 -0300

    Making the create async function to use await in the const database

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,8 +3,10 @@ const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password, isAdmin, adress } = request.body;
+
+    const database = await sqliteConnection();
   }
 }
 
