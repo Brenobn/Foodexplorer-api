9f00f7932b2c1028f2fe513f0a6c2d4afecfeca2
Brenobn <brenobragabn@gmail.com>
2024-01-22 20:43:06 -0300

    Using run in the database to insert and register user information

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -4,7 +4,7 @@ const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
   async create(request, response) {
-    const { name, email, password, isAdmin, adress } = request.body;
+    const { name, email, password, adress } = request.body;
 
     const database = await sqliteConnection();
     const checkUsersExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
@@ -12,6 +12,11 @@ class UsersController {
     if (checkUsersExists) {
       throw new AppError("Este e-mail já está em uso.");
     }
+
+    await database.run(
+      "INSERT INTO users (name, email, password, adress) VALUES (?, ?, ?, ?)",
+      [name, email, password, adress]
+    );
 
     return response.status(201).json();
   }
