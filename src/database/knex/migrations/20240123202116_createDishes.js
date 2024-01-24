08257c7313b64c23844c00465f167196e527cea0
Brenobn <brenobragabn@gmail.com>
2024-01-24 11:16:39 -0300

    Adjusting Knex migrations for new update

--- src/database/knex/migrations/20240123202116_createDishes.js
+++ src/database/knex/migrations/20240123202116_createDishes.js
@@ -1,13 +1,17 @@
-exports.up = kenx => knex.schema.createTable("dishes", table => {
-  table.increments("id");
-  table.text("name");
-  table.text("description");
-  table.text("image");
-  table.float("price");
-  table.text("category");
+exports.up = function(knex) {
+  return knex.schema.createTable("dishes", function (table) {
+    table.increments("id");
+    table.text("name");
+    table.text("description");
+    table.text("image");
+    table.float("price");
+    table.text("category");
 
-  table.timestamp("created_at").default(knex.fn.now());
-  table.timestamp("updated_at").default(knex.fn.now());
-});
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+  });
+};
 
-exports.down = kenx => knex.schema.dropTable("dishes");
+exports.down = function(knex) {
+  return knex.schema.dropTable("dishes");
+};
